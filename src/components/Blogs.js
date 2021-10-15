5591f2db7dc4398e7d90cf95e62e2d038cdd0fd6
ErrolNtetha <mphumier@gmail.com>
2021-10-16 01:40:15 +0200

    made some small changes

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,26 +1,18 @@
 import React, { Component } from "react";
-import BlogItems from "./BlogItems";
-import { blogData } from "./blogsData"
+
 
 export default class Blog extends Component {
   state = {
     title: "8 Things You Should Know Before Starting A New Business",
     date: "17 Feb 2021",
-    blogPost: "Starting a business especially in a pandemic era is never an easy thing."
+    blogPost: "This is is a blog post"
   }
 
   render() {
-
-    console.log(blogData)
-
     return (
       <>
         <div style={{ margin: "5rem" }}>
-          <h1> Blogs </h1>
-          <BlogItems title={this.state.title} blogPost={this.state.blogPost} date={this.state.date} />
-          <BlogItems title={this.state.title} blogPost={this.state.blogPost} date={this.state.date} />
-          <BlogItems title={this.state.title} blogPost={this.state.blogPost} date={this.state.date} />
-          <BlogItems title={this.state.title} blogPost={this.state.blogPost} date={this.state.date} />
+         
         </div>
         
       </>
